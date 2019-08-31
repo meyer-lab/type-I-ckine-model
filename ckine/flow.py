47b2ecd37cc7b77c5fd32c6cc9b117760706be75
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-08-31 16:19:36 -0700

    Flow cytometry functions from the end of Amgen (#606)
    * update flow.py
    
    * adding gatingFxns-Copy1.ipynb to collaborate
    
    * Updated flow.py: added  gating functions, will work on PCA functions
    
    * Updating flow.py with new Gating Functions, will update with PCA functions next
    
    * Update imports.py
    
    * Linting.
    
    * Updated flow.py. Linting.
    
    * Updated more linting
    
    * More linting fixes
    
    * Fixed last linting
    
    * Linting and Comments
    
    * Linting
    
    * added PCA fxns
    
    * Fixed PCA linting
    
    * Linting
    
    * Last Lint
    
    * changed loadings
    
    * new folder brian w/ two notebooks
    
    * Added titles to graphs
    
    * Readd fxns
    
    * updated collab pca and gating notebooks
    
    * updates to notebooks from 8/15
    
    * Fixed Scaling
    
    * Fixed Scaling Error
    
    * Added Colored PCA
    
    * Sped it Up
    
    * changed scatter type for PCAfxns_815
    
    * Edited Figs
    
    * changed t cells to include cd4+ only
    
    * Added gates to cell Functions
    
    * Saved Changes
    
    * Added Cell Specific PCA
    
    * Double check save
    
    * Fixed Scaling Issues
    
    * Added Protein Scatters for T's
    
    * Added PCA graph to Track cell type
    
    * updated colors on gating
    
    * updated files with comments and added to flow.py
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting
    
    * Linting AND new notebooks for gating, pca, gating+pca
    
    * Linting :)
    
    * Delete foo2.png
    
    * Delete Untitled.ipynb
    
    * Delete Well_C02_Lymphocytes.fcs
    
    * Remove images.
    
    * Remove fcs files.
    
    * Reduce notebooks.
    
    * Clean plots from other notebooks.
    
    * Remove code duplication.
    
    * Remove code duplication.
    
    * Remove more duplication.
    
    * Remove notebooks.
    
    * Update flow.py
    
    * Update flow.py

--- ckine/flow.py
+++ ckine/flow.py
@@ -3,17 +3,20 @@ This file includes various methods for flow cytometry analysis.
 """
 from pathlib import Path
 import numpy as np
+import pandas as pd
+import seaborn as sns
 from matplotlib import pyplot as plt
 from FlowCytometryTools import FCMeasurement
-from FlowCytometryTools import QuadGate
+from FlowCytometryTools import QuadGate, ThresholdGate
 import sklearn
 from sklearn.decomposition import PCA
 
-# Please note: if trying to display plots in jupyternotebook, must use: %matplotlib inline
-
 
 def importF(pathname):
-    """Import FCS files. Variable input: name of path name to file. Output is a list of Data File Names in FCT Format"""
+    """
+    Import FCS files. Variable input: name of path name to file. Output is a list of Data File Names in FCT Format
+    Title/file names are returned in the array file --> later referenced in other functions as title/titles input argument
+    """
     # Declare arrays and int
     file = []
     sample = []
@@ -23,34 +26,40 @@ def importF(pathname):
     for path in pathlist:
         path_in_str = str(path)
         file.append(path_in_str)
+    file.sort()
     # Go through each file and assign the file contents to entry in the array sample
     for entry in file:
         sample.append(FCMeasurement(ID="Test Sample" + str(z), datafile=entry))
         z += 1
-    importF.sample = sample
     # Returns the array sample which contains data of each file in folder (one file per entry in array)
-    return sample
+    return sample, file
 
 
 # *********************************** Gating Fxns *******************************************
 # Treg and NonTreg
 
 # add which channels relate to the proteins
+def cd4():
+    """Function for gating CD4+ cells (generates T cells)"""
+    cd41 = ThresholdGate(6.514e+03, ('VL4-H'), region="above", name='cd41')
+    cd42 = ThresholdGate(7.646e+03, ('VL4-H'), region="below", name='cd42')
+    cd4_gate = cd41 & cd42
+    return cd4_gate
+
+
 def treg():
-    """Function for creating and returning the T reg gate"""
-    # T reg: Take quad gates for T reg cells and combine them to create single, overall T reg gate
-    treg1 = QuadGate((7.063e03, 3.937e03), ("BL1-H", "VL1-H"), region="top left", name="treg1")
-    treg2 = QuadGate((5.412e03, 6.382e03), ("BL1-H", "VL1-H"), region="bottom right", name="treg2")
-    treg_gate = treg1 & treg2
+    """Function for creating and returning the T reg gate on CD4+ cells"""
+    treg1 = QuadGate((4.814e+03, 3.229e+03), ('BL1-H', 'VL1-H'), region='top right', name='treg1')
+    treg2 = QuadGate((6.258e+03, 5.814e+03), ('BL1-H', 'VL1-H'), region='bottom left', name='treg2')
+    treg_gate = treg1 & treg2 & cd4()
     return treg_gate
 
 
 def nonTreg():
-    """Function for creating and returning the Non T reg gate"""
-    # non T reg: Take quad gates for non T reg cells and combine to create overall non T reg gate
-    nonTreg1 = QuadGate((5.233e03, 1.731e03), ("BL1-H", "VL1-H"), region="top left", name="nonTreg1")
-    nonTreg2 = QuadGate((2.668e03, 5.692e03), ("BL1-H", "VL1-H"), region="bottom right", name="nonTreg2")
-    nonTreg_gate = nonTreg1 & nonTreg2
+    """Function for creating and returning the non T reg gate on CD4+ cells"""
+    nontreg1 = QuadGate((5.115e+03, 3.470e+02), ('BL1-H', 'VL1-H'), region="top left", name='nontreg1')
+    nontreg2 = QuadGate((2.586e+03, 5.245e+03), ('BL1-H', 'VL1-H'), region="bottom right", name='nontreg2')
+    nonTreg_gate = nontreg1 & nontreg2 & cd4()
     return nonTreg_gate
 
 
@@ -109,13 +118,17 @@ def rawData(sample_i, gate):
     return cell_data
 
 
-def tcells(sample_i, treg_gate, nonTreg_gate, i):
+def tcells(sample_i, treg_gate, nonTreg_gate, title):
     """
-    Function that is used to plot the Treg and NonTreg gates. Treg (yellow) and Non Treg (green). sample_i is an indivual flow cytommetry
-    file/data.
+    Function that is used to plot the Treg and NonTreg gates in CD4+ cells. Treg (yellow) and Non Treg (green). sample_i is an indivual flow cytommetry file/data.
     """
+    # Data to use is on CD4+ cells
+    # Apply new T reg and Non treg gate
     # Assign data of current file for analysis to variable smpl and transform to log scale
-    smpl = sample_i.transform("hlog", channels=["BL1-H", "VL1-H"])
+    smpl = sample_i.transform('hlog', channels=["VL4-H", "BL1-H", "VL1-H"])
+    # Create data set to only include CD4 cells
+    cd4_gate = cd4()
+    cd4_cells = smpl.gate(cd4_gate)
     # CD25 v. Foxp33: VL1 v. BL1
     # Treg
     # Apply T reg gate to overall data --> step that determines which cells are Treg
@@ -125,22 +138,23 @@ def tcells(sample_i, treg_gate, nonTreg_gate, i):
     nonTreg_cells = smpl.gate(nonTreg_gate)
 
     # Declare figure and axis
-    _, ax = plt.subplots()  # ???????? unused fig, but need to leave there to make sure right assignment for ax?
+    _, ax = plt.subplots()
     # Plot the treg gate
-    treg_cells.plot(["BL1-H", "VL1-H"], color="y")
+    treg_cells.plot(["BL1-H", "VL1-H"], color="teal")
     # Plot the non Treg gate
-    nonTreg_cells.plot(["BL1-H", "VL1-H"], color="g")
+    nonTreg_cells.plot(["BL1-H", "VL1-H"], color="cyan")
     # Plot all of the cells in the file
-    print(i)
-    ax.set_title("T Reg + Non T Reg - Gating - File " + str(i), fontsize=20)
-    smpl.plot(["BL1-H", "VL1-H"])
-
-    bar_T = ax.bar(np.arange(0, 10), np.arange(1, 11), color="y")
-    bar_NT = ax.bar(np.arange(0, 10), np.arange(30, 40), bottom=np.arange(1, 11), color="g")
+    ax.set_title("T Reg + Non T Reg - Gating - " + str(title), fontsize=12)
+    cd4_cells.plot(["BL1-H", "VL1-H"])
+    plt.xlabel("Foxp3", fontsize=12)
+    plt.ylabel("CD25", fontsize=12)
+    # Set values for legend
+    bar_T = ax.bar(np.arange(0, 10), np.arange(1, 11), color="teal")
+    bar_NT = ax.bar(np.arange(0, 10), np.arange(30, 40), bottom=np.arange(1, 11), color="cyan")
     ax.legend([bar_T, bar_NT], ("T Reg", "Non T Reg"), loc="upper left")
 
 
-def nk_bnk_plot(sample_i, nk_gate, bnk_gate, i):
+def nk_bnk_plot(sample_i, nk_gate, bnk_gate, title):
     """
     Function that plots the graph of NK and Bright NK cells (both are determined by same x, y-axis). Arguemnt 1: current sample (a single file).
     Argument 2: the gate for NK. Argument 3: the gate for bright NK.
@@ -156,7 +170,7 @@ def nk_bnk_plot(sample_i, nk_gate, bnk_gate, i):
     bnk_cells = smpl.gate(bnk_gate)
 
     _, ax1 = plt.subplots()
-    ax1.set_title("CD56 BrightNK + NK - Gating - File " + str(i), fontsize=20)
+    ax1.set_title("CD56 BrightNK + NK - Gating - " + str(title), fontsize=12)
     nk_cells.plot(["BL1-H", "VL4-H"], color="y", label="NK")
     bnk_cells.plot(["BL1-H", "VL4-H"], color="g", label="Bright NK")
     smpl.plot(["BL1-H", "VL4-H"])
@@ -166,7 +180,7 @@ def nk_bnk_plot(sample_i, nk_gate, bnk_gate, i):
     ax1.legend([bar_NK, bar_BNK], ("NK", "Bright NK"), loc="upper left")
 
 
-def cd_plot(sample_i, cd_gate, i):
+def cd_plot(sample_i, cd_gate, title):
     """
     Function that plots the graph of CD cells. Argument 1: current sample (a single file). Argument 2: the gate for CD cells. Argument 3: the value
     of the current i in a for loop --> use
@@ -179,7 +193,7 @@ def cd_plot(sample_i, cd_gate, i):
     cd_cells = smpl.gate(cd_gate)
 
     _, ax2 = plt.subplots()
-    ax2.set_title("CD3+CD8+ - Gating - File " + str(i), fontsize=20)
+    ax2.set_title("CD3+CD8+ - Gating - " + str(title), fontsize=20)
     cd_cells.plot(["RL1-H", "VL4-H"], color="b")
     smpl.plot(["RL1-H", "VL4-H"])
 
@@ -205,25 +219,29 @@ def count_data(sampleType, gate):
     return count_array, data_array
 
 
-def plotAll(sampleType, check, gate1, gate2):
+def plotAll(sampleType, check, gate1, gate2, titles):
     """
     Ask the user to input 't' for t cell, 'n' for nk cell, and 'c' for cd cell checks are used to determine if user input a T-cell, NK-cell, or
     CD-cell gate automates the process for plotting multiple files.
     """
     if check == "t":
         for i, sample in enumerate(sampleType):
-            tcells(sample, gate1, gate2, i)
+            title = titles[i].split("/")
+            title = title[len(title)-1]
+            tcells(sample, gate1, gate2, title)
     elif check == "n":
         for i, sample in enumerate(sampleType):
-            nk_bnk_plot(sample, gate1, gate2, i)
+            title = titles[i].split("/")
+            title = title[len(title)-1]
+            nk_bnk_plot(sample, gate1, gate2, title)
     elif check == "c":
         for i, sample in enumerate(sampleType):
-            cd_plot(sample, gate1, i)
+            title = titles[i].split("/")
+            title = title[len(title)-1]
+            cd_plot(sample, gate1, title)
 
 
-# ********************************** PCA Functions****************************************************
-
-
+#********************************** PCA Functions****************************************************
 def sampleT(smpl):
     """Output is the T cells data (the protein channels related to T cells)"""
     # Features are the protein channels of interest when analyzing T cells
@@ -269,7 +287,7 @@ def appPCA(data, features):
     return xf, loading
 
 
-def pcaPlt(xf, pstat, features, i):
+def pcaPlt(xf, pstat, features, title):
     """
     Used to plot the score graph.
     Scattered point color gradients are based on range/abundance of pSTAT5 data. Light --> Dark = Less --> More Active
@@ -282,23 +300,34 @@ def pcaPlt(xf, pstat, features, i):
     # Setting x and y values from xf
     x = xf[:, 0]
     y = xf[:, 1]
-    # Working with pSTAT5 data --> setting min and max values
+    # Saving numerical values for pSTAT5 data
     pstat_data = pstat.values
-
     # Creating a figure for both scatter and mesh plots for PCA
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_xlabel("Principal Component 1", fontsize=15)
-    ax.set_ylabel("Principal Component 2", fontsize=15)
-    ax.set_title(name + " - PCA - File " + str(i), fontsize=20)
-    ax.set(xlim=(-5, 5), ylim=(-5, 5))
-
     # This is the scatter plot of the cell clusters colored by pSTAT5 data
     # lighter --> darker = less --> more pSTAT5 present
-    plt.scatter(x, y, s=0.1, c=np.squeeze(pstat_data), cmap="Greens")
+    # Creating correct dimensions
+    pstat = np.squeeze(pstat_data)
+    # Determining overall pSTAT5 for a certain fcs data set
+    pstat_mean = np.mean(pstat)
+    print("mean pStat activity: ", pstat_mean)
+    # Creating a data from of x, y, and pSTAT5 in order to graph using seaborn
+    combined = np.stack((x, y, pstat)).T
+    df = pd.DataFrame(combined, columns=["PC1", "PC2", "pSTAT5"])
+    # Creating plot using seaborn. Cool note: virdis is visible for individuals who are colorblind.
+    _, ax = plt.subplots(figsize=(8, 8))
+    ax.set_title(name + " - PCA - " + str(title), fontsize=20)
+    plt.xlim(-4, 6)
+    plt.ylim(-4, 4)
+    sns.scatterplot(x="PC1", y="PC2", hue="pSTAT5", palette="viridis", data=df, s=5, ax=ax, legend=False, hue_norm=(3000, 7000))
+    ax.set_xlabel("PC1", fontsize=15)
+    ax.set_ylabel("PC2", fontsize=15)
+    # Graph the Points
+    points = plt.scatter(df["PC1"], df["PC2"], c=df["pSTAT5"], s=0, cmap="viridis", vmin=3000, vmax=7000) #set style options
+    #add a color bar
+    plt.colorbar(points)
 
 
-def loadingPlot(loading, features, i):
+def loadingPlot(loading, features, i, title):
     """Plot the loading data"""
     # Loading
     # Create graph for loading values
@@ -333,10 +362,11 @@ def loadingPlot(loading, features, i):
             if feature == "BL1-H":
                 feature = "CD56"
         plt.annotate(str(feature), xy=(x_load[z], y_load[z]))
-    ax.set_title(name + " - Loading - File " + str(i), fontsize=20)
+        plt.savefig('loading'+str(i)+'.png')
+    ax.set_title(name + " - Loading - " + str(title), fontsize=20)
 
 
-def pcaAll(sampleType, check):
+def pcaAll(sampleType, check, titles):
     """
     Use to plot the score and loading graphs for PCA. Assign protein and pstat5 arrays AND score and loading arrays
     This is all the data for each file.
@@ -353,20 +383,150 @@ def pcaAll(sampleType, check):
     # using the functions created above for a singular file
     if check == "t":
         for i, sample in enumerate(sampleType):
+            title = titles[i].split("/")
+            title = title[len(title)-1]
             data, pstat, features = sampleT(sample)
             data_array.append(data)
             pstat_array.append(pstat)
             xf, loading = appPCA(data, features)
             xf_array.append(xf)
             loading_array.append(loading)
-            pcaPlt(xf, pstat, features, i)
-            loadingPlot(loading, features, i)
+            pcaPlt(xf, pstat, features, title)
+            loadingPlot(loading, features, i, title)
+            plt.show()
     elif check == "n":
         for i, sample in enumerate(sampleType):
+            title = titles[i].split("/")
+            title = title[len(title)-1]
             data, pstat, features = sampleNK(sample)
             data_array.append(data)
             pstat_array.append(pstat)
             xf, loading = appPCA(data, features)
-            pcaPlt(xf, pstat, features, i)
-            loadingPlot(loading, features, i)
+            pcaPlt(xf, pstat, features, title)
+            loadingPlot(loading, features, i, title)
+            plt.show()
+    return data_array, pstat_array, xf_array, loading_array
+
+#************************PCA by color (gating+PCA)******************************
+
+def sampleTcolor(smpl):
+    """Output is the T cells data (the protein channels related to T cells)"""
+    # Features are the protein channels of interest when analyzing T cells
+    features = ["BL1-H", "VL1-H", "VL4-H", "BL3-H"]
+    tregd = []
+    tregp = []
+    # Transform to put on log scale
+    tform = smpl.transform("hlog", channels=["BL1-H", "VL1-H", "VL4-H", "BL3-H", "RL1-H"])
+    # Save the data of each column of the protein channels
+    data = tform.data[["BL1-H", "VL1-H", "VL4-H", "BL3-H"]][0:]
+    # Save pSTAT5 data
+    pstat = tform.data[["RL1-H"]][0:]
+    # Create a section for assigning colors to each data point of each cell population --> in this case, T cells
+    colmat = []*(len(data)+1)
+    for  i in range(len(data)):
+        if data.iat[i, 0] > 4.814e+03 and data.iat[i, 0] < 6.258e+03 and data.iat[i, 1] > 3.229e+03 and data.iat[i, 1] < 5.814e+03:
+            colmat.append('r') #Treg
+            tregd.append(data.iloc[[i]])
+            tregp.append(pstat.iloc[[i]])
+        elif data.iat[i, 0] > 2.586e+03 and data.iat[i, 0] < 5.115e+03 and data.iat[i, 1] > 3.470e+02 and data.iat[i, 1] < 5.245e+03:
+            colmat.append('g') # non Treg
+        else:
+            colmat.append('c')
+    return data, pstat, features, colmat
+
+
+def sampleNKcolor(smpl):
+    """Output is the NK cells data (the protein channels related to NK cells)"""
+    # For NK, the data consists of different channels so the data var. output will be different
+    # Output is data specific to NK cells
+    # Features for the NK file of proteins (CD3, CD8, CD56)
+    features = ["VL4-H", "RL1-H", "BL1-H"]
+    # Transform all proteins (including pSTAT5)
+    tform = smpl.transform("hlog", channels=["VL4-H", "RL1-H", "BL1-H", "BL2-H"])
+    # Assign data of three protein channels AND pSTAT5
+    data = tform.data[["VL4-H", "RL1-H", "BL1-H"]][0:]
+    pstat = tform.data[["BL2-H"]][0:]
+    # Create a section for assigning colors to each data point of each cell population --> in this case NK cells
+    colmat = []*(len(data)+1)
+    for i in range(len(data)):
+        if data.iat[i, 0] > 5.550e03 and data.iat[i, 0] < 6.468e03 and data.iat[i, 2] > 4.861e03 and data.iat[i, 2] < 5.813e03:
+            colmat.append('r') #nk
+        elif data.iat[i, 0] > 6.533e03 and data.iat[i, 0] < 7.34e03 and data.iat[i, 2] > 4.899e03 and data.iat[i, 2] < 5.751e03:
+            colmat.append('g') #bnk
+        else:
+            colmat.append('c')
+    return data, pstat, features, colmat
+
+
+def pcaPltColor(xf, pstat, features, title, colormat):
+    """
+    Used to plot the score graph.
+    Scattered point color gradients are based on range/abundance of pSTAT5 data. Light --> Dark = Less --> More Active
+    """
+    # PCA
+    if len(features) == 4:
+        name = "T Cells"
+    elif len(features) == 3:
+        name = "NK Cells"
+    # Setting x and y values from xf
+    x = xf[:, 0]
+    y = xf[:, 1]
+    # Working with pSTAT5 data --> setting min and max values
+    pstat_data = pstat.values
+    pstat_mean = np.mean(pstat_data)
+    print("mean pStat activity: ", pstat_mean)
+    plt.show()
+    # Creating a figure for both scatter and mesh plots for PCA
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlabel("Principal Component 1", fontsize=12)
+    ax.set_ylabel("Principal Component 2", fontsize=12)
+    ax.set_title(name + " - PCA - " + str(title), fontsize=20)
+    ax.set(xlim=(-5, 5), ylim=(-5, 5))
+    # This is the scatter plot of the cell clusters colored cell type
+    colormat = np.array(colormat)
+    plt.scatter(x[colormat == "c"], y[colormat == "c"], s=.15, c="c", label="Other", alpha=0.5)
+    plt.scatter(x[colormat == "g"], y[colormat == "g"], s=.15, c="g", label="NonTreg", alpha=0.5)
+    plt.scatter(x[colormat == "r"], y[colormat == "r"], s=.15, c="r", label="TReg", alpha=0.5)
+    plt.legend()
+
+
+def pcaAllCellType(sampleType, check, titles):
+    """
+    Use to plot the score and loading graphs for PCA. Assign protein and pstat5 arrays AND score and loading arrays
+    This is all the data for each file.
+    Want to use for both T and NK cells? Use it twice!
+    sampleType is importF for T or NK
+    check == "t" for T cells OR check == "n" for NK cells
+    """
+    # declare the arrays to store the data
+    data_array = []
+    pstat_array = []
+    xf_array = []
+    loading_array = []
+    # create the for loop to file through the data and save to the arrays
+    # using the functions created above for a singular file
+    if check == "t":
+        for i, sample in enumerate(sampleType):
+            title = titles[i].split("/")
+            title = title[len(title)-1]
+            data, pstat, features, colormat = sampleTcolor(sample)
+            data_array.append(data)
+            pstat_array.append(pstat)
+            xf, loading = appPCA(data, features)
+            xf_array.append(xf)
+            loading_array.append(loading)
+            pcaPltColor(xf, pstat, features, title, colormat) #changed
+            loadingPlot(loading, features, i, title)
+    elif check == "n":
+        for i, sample in enumerate(sampleType):
+            title = titles[i].split("/")
+            title = title[len(title)-1]
+            data, pstat, features, colormat = sampleNKcolor(sample)
+            data_array.append(data)
+            pstat_array.append(pstat)
+            xf, loading = appPCA(data, features)
+            xf_array.append(xf)
+            pcaPltColor(xf, pstat, features, title, colormat)
+            loadingPlot(loading, features, i, title)
     return data_array, pstat_array, xf_array, loading_array
