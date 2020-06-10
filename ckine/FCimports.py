e1252e6daafff06c3f32b1488bac37b5a6225785
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2020-06-10 13:09:59 -0700

    One function (#709)
    * One function
    
    * autopep8 action fixes (#710)
    
    Co-authored-by: Aaron Meyer <aarmey@users.noreply.github.com>
    
    * Lint
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Aaron Meyer <aarmey@users.noreply.github.com>

--- ckine/FCimports.py
+++ ckine/FCimports.py
@@ -1,11 +1,12 @@
 """
 This file includes various methods for flow cytometry analysis of fixed cells.
 """
+import ast
 import os
-from os.path import dirname
+from os.path import dirname, join
 from pathlib import Path
-import numpy as np
 import pandas as pd
+import numpy as np
 from FlowCytometryTools import FCMeasurement
 
 path_here = dirname(dirname(__file__))
@@ -19,7 +20,7 @@ def combineWells(samples):
     return combinedSamples
 
 
-def importF(date, plate, wellRow, panel, channels, wellNum=None):
+def importF(date, plate, wellRow, panel, wellNum=None):
     """
     Import FCS files. Variable input: date in format mm-dd, plate #, panel #, and well letter. Output is a list of Data File Names in FCT Format
     Title/file names are returned in the array file --> later referenced in other functions as title/titles input argument
@@ -51,11 +52,11 @@ def importF(date, plate, wellRow, panel, channels, wellNum=None):
 
     if wellNum is None:
         combinedSamples = combineWells(sample)  # Combines all files from samples
-        combinedSamples = subtract_unstained_signal(combinedSamples, channels, unstainedWell)  # Subtracts background
-        return combinedSamples.transform("hlog", channels=channels)  # Transforms and returns
+        compSample = applyMatrix(combinedSamples, compMatrix(date, plate, wellRow))  # Applies compensation matrix
+        return compSample, unstainedWell
 
-    tsample = subtract_unstained_signal(sample[wellNum - 1], channels, unstainedWell)
-    return tsample.transform("hlog", channels=channels)
+    compSample = applyMatrix(sample, compMatrix(date, plate, wellRow))
+    return compSample, unstainedWell
 
 
 def subtract_unstained_signal(sample, channels, unstainedWell):
@@ -108,3 +109,89 @@ def applyMatrix(sample, matrix):
     sample.data = sample.data.dot(matrix)  # Use matrix multiplication to compensate the relevant data
     sample.data = sample.data.join(holder)  # Restore uncompensated channels to sample
     return sample
+
+
+def import_gates():
+    """ Imports dataframe with gates for all cell types and replicates. """
+    data = pd.read_csv(join(path_here, "ckine/data/fc_gates.csv"))
+    data.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)
+    return data
+
+
+def apply_gates(date, plate, gates_df, subpopulations=False):
+    """ Constructs dataframe with channels relevant to receptor quantification. """
+    df, unstainedWell = samp_Gate(date, plate, gates_df, 'T-helper', subPop=subpopulations)
+    df = df.append(samp_Gate(date, plate, gates_df, 'T-reg', subPop=subpopulations)[0])
+    df = df.append(samp_Gate(date, plate, gates_df, 'NK', subPop=subpopulations)[0])
+    df = df.append(samp_Gate(date, plate, gates_df, 'CD8+', subPop=subpopulations)[0])
+    # All samples for data and plate processed combined
+    df = subtract_unstained_signal(df, ["VL1-H", "BL5-H", "RL1-H"], unstainedWell)
+    # Background signal substracted
+    return df
+
+
+def samp_Gate(date, plate, gates_df, cellType, subPop=False):
+    """ Returns gated sample for a given date and plate. """
+    # import data and create transformed df for gating
+    tchannels, subPopName, row, panelNum = cellGateDat(cellType)
+    panel, unstainedWell = importF(date, plate, row, panelNum)
+    panel_t = panel.transform("tlog", channels=tchannels)  # Creates copy of panel1 to transform and gate
+
+    df = pd.DataFrame(columns=["Cell Type", "Date", "Plate", "VL1-H", "BL5-H", "RL1-H"])
+
+    # implement gating, revert tlog, and add to dataframe
+    if cellType in ('T-reg', 'T-helper'):
+        samplecd3cd4 = panel_t.gate(ast.literal_eval(gates_df.loc[(gates_df["Name"] == 'CD3CD4') &
+                                                                  (gates_df["Date"] == date) & (gates_df["Plate"] == float(plate))]["Gate"].values[0]))
+        sample = samplecd3cd4.gate(ast.literal_eval(gates_df.loc[(gates_df["Name"] == cellType) &
+                                                                 (gates_df["Date"] == date) & (gates_df["Plate"] == float(plate))]["Gate"].values[0]))
+    else:
+        sample = panel_t.gate(ast.literal_eval(gates_df.loc[(gates_df["Name"] == cellType) &
+                                                            (gates_df["Date"] == date) & (gates_df["Plate"] == float(plate))]["Gate"].values[0]))
+    # Gated signals based on gating values from csv
+    gated_idx = np.array(sample.data.index)
+    panel.set_data(panel.data.loc[gated_idx])  # Selects only the corresponding data points from panel1(untransformed) based on gated points from panel1_t
+    df_add = pd.DataFrame({"Cell Type": np.tile(cellType, sample.counts), "Date": np.tile(date, sample.counts), "Plate": np.tile(plate, sample.counts),
+                           "VL1-H": panel.data[["VL1-H"]].values.reshape((sample.counts,)), "BL5-H": panel.data[["BL5-H"]].values.reshape((sample.counts,)),
+                           "RL1-H": panel.data[["RL1-H"]].values.reshape((sample.counts,))})
+    df = df.append(df_add)
+
+    # separates memory and naive populations and adds to dataframe
+    if subPop:
+        for subpopulation in subPopName:
+            sampleSub = sample.gate(ast.literal_eval(gates_df.loc[(gates_df["Name"] == subpopulation) &
+                                                                  (gates_df["Date"] == date) & (gates_df["Plate"] == float(plate))]["Gate"].values[0]))
+            gated_idx = np.array(sampleSub.data.index)
+            panel_S = panel.data.loc[gated_idx]
+            df_add = pd.DataFrame({"Cell Type": np.tile(subpopulation, sampleSub.counts), "Date": np.tile(date, sampleSub.counts), "Plate": np.tile(plate, sampleSub.counts),
+                                   "VL1-H": panel_S.data[["VL1-H"]].values.reshape((sampleSub.counts,)), "BL5-H": panel_S.data[["BL5-H"]].values.reshape((sampleSub.counts,)),
+                                   "RL1-H": panel_S.data[["RL1-H"]].values.reshape((sampleSub.counts,))})
+            df = df.append(df_add)
+
+    return df, unstainedWell
+
+
+def cellGateDat(cellType):
+    "Returns pertinent gating information for a given cell type"
+    if cellType in ('T-reg', 'T-helper'):
+        tchannels = ['VL6-H', 'VL4-H', 'BL1-H', 'VL1-H', 'BL3-H']
+        row = 'A'
+        panel = 1
+        if cellType == "T-reg":
+            subPopName = ["Mem Treg", "Naive Treg"]
+        else:
+            subPopName = ['Naive Th', 'Mem Th']
+    elif cellType == "CD8+":
+        tchannels = ['VL4-H', 'VL6-H', 'BL3-H']
+        row = 'C'
+        panel = 3
+        subPopName = ['Naive CD8+', 'Mem CD8+']
+    elif cellType == "NK":
+        tchannels = ['VL4-H', 'BL3-H']
+        row = 'B'
+        panel = 2
+        subPopName = ['NKT']
+
+    assert tchannels != []
+
+    return tchannels, subPopName, row, panel
