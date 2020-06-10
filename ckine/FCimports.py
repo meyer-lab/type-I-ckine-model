3e88c7a7b71911b85791c81310db38a9c39f8177
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2020-06-09 20:10:18 -0700

    Compensation (#704)
    * First look
    
    * Lint
    
    * Reorder mat
    
    * autopep8 action fixes (#705)
    
    Co-authored-by: Aaron Meyer <aarmey@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Aaron Meyer <aarmey@users.noreply.github.com>

--- ckine/FCimports.py
+++ ckine/FCimports.py
@@ -5,6 +5,7 @@ import os
 from os.path import dirname
 from pathlib import Path
 import numpy as np
+import pandas as pd
 from FlowCytometryTools import FCMeasurement
 
 path_here = dirname(dirname(__file__))
@@ -63,4 +64,47 @@ def subtract_unstained_signal(sample, channels, unstainedWell):
         meanBackground = np.mean(unstainedWell.data[channel])  # Calculates mean unstained signal for given channel
         sample[channel] = np.maximum(sample[channel] - meanBackground, 0.0)
 
+    return sample
+
+
+def compMatrix(date, plate, panel, invert=True):
+    """Creates compensation matrix given parameters date in mm-dd, plate number and panel A, B, or C."""
+    path = path_here + "/ckine/data/compensation/0" + date + "/Plate " + plate + "/Plate " + plate + " - " + panel + ".csv"
+    # imports csv file with comp values as a dataframe
+    header_names = ['Channel1', 'Channel2', 'Comp']
+    df_comp = pd.read_csv(path, header=None, skiprows=1, names=header_names)
+    # Add diangonal values of 100 to compensation values
+    addedChannels = []
+    for i in df_comp.index:
+        channelName = df_comp.iloc[i]['Channel1']
+        if channelName not in addedChannels:  # Ensures a diagonal value is only added once for each channel
+            addedChannels.append(channelName)
+            df2 = pd.DataFrame([[channelName, channelName, 100]], columns=['Channel1', 'Channel2', 'Comp'])  # Creates new row for dataframe
+            df_comp = df_comp.append(df2, ignore_index=True)  # Adds row
+    # create square matrix from compensation values
+    df_matrix = pd.DataFrame(index=addedChannels, columns=addedChannels)  # df_matrix is now a square and has exactly one row and one column for each channel
+    for i in df_matrix.index:
+        for c in df_matrix.columns:
+            df_matrix.at[i, c] = df_comp.loc[(df_comp['Channel1'] == c) & (df_comp['Channel2'] == i), 'Comp'].iloc[0]  # Fills in square matrix by finding corresponding comp value from csv
+    # df_matrix now has all values in square matrix form
+    df_matrix = df_matrix.div(100)
+    if invert:  # true by default, inverts matrix before returning it
+        a = np.matrix(df_matrix.values, dtype=float)  # Convert to np to allow for linalg usage
+        df_matrix = pd.DataFrame(np.linalg.inv(a), df_matrix.columns, df_matrix.index)  # Calculate inverse and put pack as dataframe
+    return df_matrix
+
+
+def applyMatrix(sample, matrix):
+    """Multiples two matrices together in the order sample dot matrix"""
+    holder = pd.DataFrame()  # Will hold columns not being compensated
+    for c in sample.data.columns:
+        if c not in matrix:  # If sample channel column is not found in matrix
+            holder = holder.join(sample.data[[c]], how='right')  # Store for after calculation
+            sample.data = sample.data.drop([c], axis=1)  # Removed column to allow for matrix multiplication
+
+    cols = sample.data.columns
+    matrix = matrix[cols]
+    matrix = matrix.reindex(cols)
+    sample.data = sample.data.dot(matrix)  # Use matrix multiplication to compensate the relevant data
+    sample.data = sample.data.join(holder)  # Restore uncompensated channels to sample
     return sample
